0f136bd44d7276e482279bc15d75445cdec4083e
Chris Hartwig <chris@chris-hartwig.com>
2021-01-04 14:25:19 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/chrisDeFouRire/gitv
+
+go 1.13
+
+require github.com/go-git/go-git/v5 v5.2.0
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/go-git/go-git/v5"
+)
+
+func main() {
+
+	repo, err := git.PlainOpen(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tags, err := repo.Tags()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tags.Close()
+
+	for ref, err := tags.Next(); err == nil; {
+		log.Print(ref.Name().String())
+	}
+}
