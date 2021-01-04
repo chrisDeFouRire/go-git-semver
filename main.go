9050a7ffb5277120a17825d0d54d02b83cc93b31
Chris Hartwig <chris@chris-hartwig.com>
2021-01-04 16:01:57 +0000

    finds and parses tag

--- main.go
+++ main.go
@@ -3,20 +3,34 @@ package main
 import (
 	"fmt"
 	"log"
+	"regexp"
+	"strconv"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
 )
 
+var (
+	// ErrNotSemver if a tag can't be parsed as a semantic versioning tag
+	ErrNotSemver = fmt.Errorf("Tag is not SEMVER")
+	// ErrCantParseNumber if a tag number can't be parsed
+	ErrCantParseNumber = fmt.Errorf("Tag number can't be parsed")
+
+	re = regexp.MustCompile(`^v([0-9]+)\.([0-9]+)\.([0-9]+)-?(.*)$`)
+)
+
 func main() {
+
+	tagList := make(map[plumbing.Hash]string)
 
 	repo, err := git.PlainOpen(".")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	/* Get all tags
+	/* Get all tags by hash */
 
+	log.Println(">> find TAGS ")
 	tags, err := repo.Tags()
 	if err != nil {
 		log.Fatal(err)
@@ -25,22 +39,43 @@ func main() {
 
 	for ref, err := tags.Next(); err == nil; ref, err = tags.Next() {
 		tagName := ref.Name().Short()
-		log.Print(tagName)
-	}
-	*/
-
-	head, err := repo.Head()
-	if err != nil {
-		log.Fatal(err)
+		tagList[ref.Hash()] = tagName
 	}
 
-	refs, _ := repo.References()
-	refs.ForEach(func(ref *plumbing.Reference) error {
-		if ref.Type() == plumbing.HashReference && ref.Hash() == head.Hash() {
-			fmt.Println(ref)
-			fmt.Println(ref.Name().Short())
+	log.Println(">> Inspect log")
+
+	iter, err := repo.Log(&git.LogOptions{})
+	for ref, err := iter.Next(); err == nil; ref, err = iter.Next() {
+		tag, found := tagList[ref.Hash]
+		if found {
+			major, minor, patch, rest, err := tag2Semver(tag)
+
+			if err == nil {
+				log.Printf("Version %d.%d.%d-%s", major, minor, patch, rest)
+				break
+			}
 		}
+	}
 
-		return nil
-	})
+}
+
+func tag2Semver(tag string) (int, int, int, string, error) {
+	found := re.FindSubmatch([]byte(tag))
+	if found == nil {
+		return 0, 0, 0, "", ErrNotSemver
+	}
+	major, err := strconv.Atoi(string(found[1]))
+	if err != nil {
+		return 0, 0, 0, "", ErrCantParseNumber
+	}
+	minor, err := strconv.Atoi(string(found[2]))
+	if err != nil {
+		return 0, 0, 0, "", ErrCantParseNumber
+	}
+	patch, err := strconv.Atoi(string(found[3]))
+	if err != nil {
+		return 0, 0, 0, "", ErrCantParseNumber
+	}
+
+	return major, minor, patch, string(found[4]), nil
 }
